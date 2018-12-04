4cf96d1dfc39d52d067ebc20bc366733539c9fec
root <root@localhost.localdomain>
2018-12-04 01:00:30 -0300

    tests que faltaban

--- test.rb
+++ test.rb
@@ -35,8 +35,22 @@ class MiniMarketTest < MiniTest::Test
 
 	def test_add_wrong_item
 		data = '{"sku": "12345-WH-XS", "description": "Women hoodie - White - XS", "price": 23.48}'
-		post '/items.json', JSON.parse(data) 
+		post '/items.json', data
 		assert_equal 422, last_response.status
 	end
 
+    def test_get_cart
+        get '/cart/pepe123.json'
+        assert_equal 200, last_response.status
+        assert last_response.ok?
+        assert_equal '{"usuario":"pepe123","fecha":"'+Date.today.to_s+'","monto":0}', last_response.body
+    end
+
+    def test_add_item_to_cart
+        put '/cart/pepe123.json', '{"item_id":1,"cantidad":2}'
+        assert_equal 200, last_response.status
+        assert last_response.ok?
+        get '/cart/pepe123.json'
+        assert_equal '{"usuario":"pepe123","fecha":"'+Date.today.to_s+'","monto":50.8}', last_response.body
+    end
 end
